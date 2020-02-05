cd16a54ea1f2082c35f8753450cbdca161cadf8a
Wecros <wecros@gmail.com>
2020-02-05 23:16:20 +0100

    Add alternating turns, game over when board is full
    todo: make input foolproof
          make logic for winning

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -25,21 +25,54 @@ def reset_game_board(game_board):
         [0, 0, 0]]
 
 def print_game_board(game_board):
+    char_count = 0
     for line in game_board:
-        print(line)
+        for char in line:
+            if char_count % 3 != 0:
+                print(" ", end='')
+            if char == 0:
+                print("-", end='')
+            elif char == 1:
+                print(CROSS_CHAR, end='')
+            elif char == 2:
+                print(CIRCLE_CHAR, end='')
+            char_count += 1
+        print("")
+
+def is_game_finished(game_board):
+    full_board = False
+    if any(0 in line for line in game_board):
+        full_board = False
+    else:
+        full_board = True
+
+    if full_board:
+        print("The board is full! The game's a draw.")
+        return True
+
+    return False
+
 
 def game():
     print_start_message()
     game_board = init_game_board()
+    turn = 1
 
     while (True):
         X_coord = int(input("Please enter the X coordinate (1-3): ")) - 1
         Y_coord = int(input("Please enter the Y coordinate (1-3): ")) - 1
         
-        game_board[Y_coord][X_coord] = 1
-
-        print_game_board(game_board)
+        if (turn % 2):
+            game_board[Y_coord][X_coord] = 1
+        else:
+            game_board[Y_coord][X_coord] = 2
         
+        print_game_board(game_board)
+        turn += 1
 
+        if (is_game_finished(game_board)):
+            break
+    print("The game is over!")
 
+    
 game()
